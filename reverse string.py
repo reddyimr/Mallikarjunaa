6bfd2682e8894c43205124b4d70ccc8de57de822
teja1600 <p.dharmateja4@gmail.com>
2017-02-17 14:21:39 +0530

    Mallikarjunaa
    Mallikarjunaa

---
+++ reverse string.py
@@ -0,0 +1,10 @@
+#a=input("enter the string")
+#b=a[::-1]
+#print (b)
+#to reverse an array
+n=int(input("enter the size of an array"))
+a=[]
+for i in range(n):
+    a.append(int(input()))
+b=a[::-1]
+print (b)
